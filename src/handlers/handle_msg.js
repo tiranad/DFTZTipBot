2409930aa96074a2e9e8741ba41ab9741479ffac
DA JUUKES <dajuukes@gmail.com>
2018-05-09 18:07:54 -0700

    working reddit comment stream

---
+++ src/handlers/handle_msg.js
@@ -0,0 +1,19 @@
+module.exports = async (post, client) => {
+    if (!post.body.startsWith('!pivxtip')) return;
+    const {parent_id, body} = post;
+    const args = body.match(/\S+/g);
+
+    if (args.length < 2) return;
+
+    const amount = parseInt(args[1]);
+    if (isNaN(amount)) return;
+
+    const c = await client.getComment(parent_id);
+
+    if (c) {
+        //do stuff with comment
+    }
+    else {
+        //error
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,21 @@
+const Snoowrap = require('snoowrap');
+const snoostream = require('snoostream');
+const config = require('./data/config.json');
+
+const msgHandler = require('./handlers/handle_msg.js');
+
+const client = new Snoowrap({
+    userAgent   : config.auth.USER_AGENT,
+    clientId    : config.auth.CLIENT_ID,
+    clientSecret: config.auth.CLIENT_SECRET,
+    username    : config.auth.USERNAME,
+    password    : config.auth.PASSWORD
+});
+
+const snooStream = snoostream(client);
+
+const commentStream = snooStream.commentStream('PIVXTipTest', {regex: /([!pivxtip])\w+/g});
+
+commentStream.on('post', (post, match) => {
+    msgHandler(post, client);
+});
